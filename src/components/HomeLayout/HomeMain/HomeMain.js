c73c228cf6ee11be436c77cf8d47d459fad9e335
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-06-15 16:10:29 +0300

    Home page finised

--- src/components/HomeLayout/HomeMain/HomeMain.js
+++ src/components/HomeLayout/HomeMain/HomeMain.js
@@ -81,20 +81,43 @@ const HomeMain = () => {
                 </section>
             </section>
             <div className={styles.speakerHover}></div>
-            <section className={styles.zx7Speakers}>
-                <section className={styles.zx7SpeakersContent}>
-                    <h4>ZX7 speaker</h4>
-                    <Link to="#">See Product</Link>
+            <div className={styles.gridWrapper}>
+                <section className={styles.zx7Speakers}>
+                    <section className={styles.zx7SpeakersContent}>
+                        <h4>ZX7 speaker</h4>
+                        <Link to="#">See Product</Link>
+                    </section>
                 </section>
-            </section>
-            <section className={styles.earphonesSection}>
                 <div className={styles.imageWrapper}>
-                    
+                    <img
+                        src="./images/home/desktop/image-earphones-yx1.jpg"
+                        alt="earphones-img"
+                    />
                 </div>
                 <section className={styles.earphonesSectionContent}>
                     <h4>YX1 earphones</h4>
                     <Link to="#">See Products</Link>
                 </section>
+            </div>
+            <section className={styles.descriptionSection}>
+                <section className={styles.descriptionSectionText}>
+                    <h2>
+                        Bringing you the <span>best</span> audio gear
+                    </h2>
+                    <p>
+                        Located at the heart of New York City, Audiophile is the
+                        premier  store for high end headphones, earphones,
+                        speakers, and audio accessories. We have a large
+                        showroom and luxury demonstration rooms available for
+                        you to browse and experience a wide range of our
+                        products. Stop by our store to meet some of the
+                        fantastic people who make Audiophile the best place to
+                        buy your portable audio equipment.
+                    </p>
+                </section>
+                <div className={styles.descriptionSectionImg}>
+                    <img src="./images/shared/desktop/image-best-gear.jpg" alt="man-listening-music" />
+                </div>
             </section>
         </main>
     );
